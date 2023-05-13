29272ffcf9324c7394a3a1ce3af3c5b4b7a56e2e
Kelechi <kanomneze@gmail.com>
2023-05-13 12:03:24 +0100

    Opertor Class

---
+++ operatorClass.js
@@ -0,0 +1,22 @@
+// let israelOrange = true;
+
+// israelOrange
+//   ? console.log("Yes, he is wearing orange.")
+//   : console.log("No, he is not wearing orange.");
+
+// israelOrange = false;
+
+// israelOrange
+//   ? console.log("Yes, he is wearing orange.")
+//   : console.log("No, he is not wearing orange.");
+
+let now = new Date();
+console.log(now);
+console.log(now.getFullYear());
+console.log(now.getMonth());
+console.log(now.getDate());
+console.log(now.getHours());
+console.log(now.getMinutes());
+console.log(now.getSeconds());
+
+console.log(now.getTime());
