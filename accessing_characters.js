32d5056547c20c2a20e3c9f415ff01004bc3add9
Kelechi <kanomneze@gmail.com>
2023-05-15 15:49:23 +0100

    string_methods

---
+++ accessing_characters.js
@@ -0,0 +1,12 @@
+let string = "Kelechi Anomneze";
+let firstLetter = string[0];
+console.log(firstLetter); //K
+let secondLetter = string[1];
+let thirdLetter = string[2];
+let lastLetter = string[9];
+let seventhLetter = string[7];
+console.log(seventhLetter); //A
+console.log(lastLetter); // e
+let lastIndex = string.length - 1;
+console.log(lastIndex); // 15
+console.log(string[lastIndex]); // e
---
+++ concat.js
@@ -0,0 +1,6 @@
+// concat(): it takes many substrings and creates concatenation.
+// string.concat(substring, substring, substring)
+let string = "PluralCode";
+console.log(string.concat(" Javascript", " February", " Cohort", " Class"));
+let country = "Great ";
+console.log(country.concat("Britain"));
