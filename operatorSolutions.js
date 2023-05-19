7de0c4fda28ac615ad85f72a765d83aa629abc7a
Kelechi <kanomneze@gmail.com>
2023-05-19 10:15:29 +0100

    no.2 & no.3

--- operatorSolutions.js
+++ operatorSolutions.js
@@ -1,10 +1,13 @@
-let firstName = "Kelechi";
-let lastName = "Anomneze";
-let country = "Nigeria";
-let city = "Lagos";
-let age = 26;
-let isMarried = false;
-let year = 2023;
+// Level 1
+
+// number 1
+let firstName = "Kelechi"; // string
+let lastName = "Anomneze"; // string
+let country = "Nigeria"; // string
+let city = "Lagos"; // string
+let age = 26; // number
+let isMarried = false; // Boolean
+let year = 2023; // number
 
 console.log(
   typeof firstName,
@@ -15,3 +18,11 @@ console.log(
   typeof isMarried,
   typeof year
 );
+
+// number 2
+let num = 10; // number not 10
+console.log(typeof num);
+
+// number 3
+let numTwo = 9.8;
+console.log(parseInt(numTwo)); // 9 not 10
