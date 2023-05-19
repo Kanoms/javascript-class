0ce236dc1d19a69d9e501d0bbba78c5a1df3f37b
Kelechi <kanomneze@gmail.com>
2023-05-19 10:05:50 +0100

    level 1 number 1

---
+++ operatorSolutions.js
@@ -0,0 +1,17 @@
+let firstName = "Kelechi";
+let lastName = "Anomneze";
+let country = "Nigeria";
+let city = "Lagos";
+let age = 26;
+let isMarried = false;
+let year = 2023;
+
+console.log(
+  typeof firstName,
+  typeof lastName,
+  typeof country,
+  typeof city,
+  typeof age,
+  typeof isMarried,
+  typeof year
+);
