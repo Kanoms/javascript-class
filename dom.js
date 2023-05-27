3eb73e2cb37fdd03dfdda0e318840f218da774f9
Kelechi <kanomneze@gmail.com>
2023-05-27 10:51:52 +0100

    DOM1

---
+++ dom.js
@@ -0,0 +1,8 @@
+const allTitles = document.getElementsByTagName("h1");
+
+console.log(allTitles); //HTMLCollections
+console.log(allTitles.length); // 4
+
+for (let i = 0; i < allTitles.length; i++) {
+  console.log(allTitles[i]); // prints each elements in the HTMLCollection
+}
