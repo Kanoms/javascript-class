821b270021fab11bc77511f002772f71dc311e1c
Kelechi <kanomneze@gmail.com>
2023-06-03 13:55:10 +0100

    test

--- test.js
+++ test.js
@@ -15,3 +15,24 @@ const txt = JSON.stringify(
 );
 
 console.log(txt);
+
+const myself = `{
+  "firstName": "Kelechi",
+  "lastName": "Anomneze",
+  "age": 26,
+  "country": "Nigeria",
+  "city": "Lagos",
+  "email": "kanomneze@gmail.com",
+  "hobbies": [
+     "watch football",
+     "playing table",
+     "coding"
+  ]
+}`;
+
+const newself = JSON.parse(myself, (key, value) => {
+  let newValue =
+    typeof value == "string" && key != "email" ? value.toUpperCase() : value;
+  return newValue;
+});
+console.log(newself);
